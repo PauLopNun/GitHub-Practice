693d7abb9c84ec9e44b318b6f490323c508d698a
GitChamoy <paulopnun09@gmail.com>
2024-10-24 17:36:31 +0200

    Update Add Java.java

--- Add Java.java
+++ Add Java.java
@@ -1,1 +1,8 @@
 // Add Java Code Here
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Print "Hello, World!" to the console
+        System.out.println("Hello, World!");
+    }
+}
